568b75787cb3edbb2ecfd402b562bbd2bb937ce5
KangarooDrop <kevinmasse@comcast.net>
2022-01-18 03:20:25 -0500

    Players cannot attempt to start/join a game with no deck files

--- Scripts/Networking/Lobby.gd
+++ Scripts/Networking/Lobby.gd
@@ -48,18 +48,23 @@ func openFileSelector():
 			files.append(file)
 	dir.list_dir_end()
 	
-	for c in $DeckSelector/VBoxContainer.get_children():
-		if c is Button and c.name != "BackButton":
-			c.queue_free()
-	for i in range(files.size()):
-		var b = Button.new()
-		$DeckSelector/VBoxContainer.add_child(b)
-		b.text = str(files[i])
-		b.connect("pressed", self, "onFileButtonClicked", [files[i]])
-		$DeckSelector/VBoxContainer.move_child(b, i+1)
-	$DeckSelector/VBoxContainer.set_anchors_and_margins_preset(Control.PRESET_CENTER)
-	$DeckSelector/Background.rect_size = $DeckSelector/VBoxContainer.rect_size + Vector2(60, 20)
-	$DeckSelector/Background.rect_position = $DeckSelector/VBoxContainer.rect_position - Vector2(30, 10)
+	if files.size() > 0:
+		for c in $DeckSelector/VBoxContainer.get_children():
+			if c is Button and c.name != "BackButton":
+				c.queue_free()
+		for i in range(files.size()):
+			var b = Button.new()
+			$DeckSelector/VBoxContainer.add_child(b)
+			b.text = str(files[i])
+			b.connect("pressed", self, "onFileButtonClicked", [files[i]])
+			$DeckSelector/VBoxContainer.move_child(b, i+1)
+		$DeckSelector/VBoxContainer.set_anchors_and_margins_preset(Control.PRESET_CENTER)
+		$DeckSelector/Background.rect_size = $DeckSelector/VBoxContainer.rect_size + Vector2(60, 20)
+		$DeckSelector/Background.rect_position = $DeckSelector/VBoxContainer.rect_position - Vector2(30, 10)
+	else:
+		MessageManager.notify("You must create a new deck before playing")
+		$DeckSelector.visible = false
+		$MultiplayerUI.visible = true
 	
 	
 	
